d544dd488f4a8f130c4f794e845ca6244e087eb0
giangthidua <125744426+giangadua06042001@users.noreply.github.com>
2023-06-09 11:43:29 +0700

    bai thu hanh 3

---
+++ src/main/java/com/example/practice/service/customer/CustomerService.java
@@ -0,0 +1,33 @@
+package com.example.practice.service.customer;
+
+import com.example.practice.model.Customer;
+import com.example.practice.repository.ICustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomerService implements ICustomerService {
+    @Autowired
+    private ICustomerRepository customerRepository;
+
+    public Iterable<Customer> findAll() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Optional<Customer> findById(Long id) {
+        return customerRepository.findById(id);
+    }
+
+    @Override
+    public void save(Customer customer) {
+        customerRepository.save(customer);
+    }
+
+    @Override
+    public void remove(Long id) {
+        customerRepository.deleteById(id);
+    }
+}
---
+++ src/main/java/com/example/practice/service/customer/ICustomerService.java
@@ -0,0 +1,8 @@
+package com.example.practice.service.customer;
+
+
+import com.example.practice.model.Customer;
+import com.example.practice.service.IGeneralService;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
